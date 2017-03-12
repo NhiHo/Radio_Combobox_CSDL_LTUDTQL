d8bdcd215b6fd33c7cf84dcafee73e80e96e6981
Hồ Xuân Ý Nhi <nhi1296@gmail.com>
2017-03-12 19:47:52 +0700

    BAI 1 - 2
    COMPLETE

---
+++ K48/K48/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace K48
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvKhoa = new System.Windows.Forms.DataGridView();
+            this.rdoKhoa = new System.Windows.Forms.RadioButton();
+            this.rdoLop = new System.Windows.Forms.RadioButton();
+            this.rdoSV = new System.Windows.Forms.RadioButton();
+            this.btnHienthi = new System.Windows.Forms.Button();
+            this.cboKhoa = new System.Windows.Forms.ComboBox();
+            this.cboLop = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvKhoa
+            // 
+            this.dgvKhoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhoa.Location = new System.Drawing.Point(12, 135);
+            this.dgvKhoa.Name = "dgvKhoa";
+            this.dgvKhoa.Size = new System.Drawing.Size(559, 283);
+            this.dgvKhoa.TabIndex = 0;
+            // 
+            // rdoKhoa
+            // 
+            this.rdoKhoa.AutoSize = true;
+            this.rdoKhoa.Location = new System.Drawing.Point(12, 12);
+            this.rdoKhoa.Name = "rdoKhoa";
+            this.rdoKhoa.Size = new System.Drawing.Size(50, 17);
+            this.rdoKhoa.TabIndex = 1;
+            this.rdoKhoa.TabStop = true;
+            this.rdoKhoa.Text = "Khoa";
+            this.rdoKhoa.UseVisualStyleBackColor = true;
+            // 
+            // rdoLop
+            // 
+            this.rdoLop.AutoSize = true;
+            this.rdoLop.Location = new System.Drawing.Point(12, 35);
+            this.rdoLop.Name = "rdoLop";
+            this.rdoLop.Size = new System.Drawing.Size(43, 17);
+            this.rdoLop.TabIndex = 2;
+            this.rdoLop.TabStop = true;
+            this.rdoLop.Text = "Lớp";
+            this.rdoLop.UseVisualStyleBackColor = true;
+            // 
+            // rdoSV
+            // 
+            this.rdoSV.AutoSize = true;
+            this.rdoSV.Location = new System.Drawing.Point(12, 58);
+            this.rdoSV.Name = "rdoSV";
+            this.rdoSV.Size = new System.Drawing.Size(39, 17);
+            this.rdoSV.TabIndex = 3;
+            this.rdoSV.TabStop = true;
+            this.rdoSV.Text = "SV";
+            this.rdoSV.UseVisualStyleBackColor = true;
+            // 
+            // btnHienthi
+            // 
+            this.btnHienthi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHienthi.Location = new System.Drawing.Point(417, 10);
+            this.btnHienthi.Name = "btnHienthi";
+            this.btnHienthi.Size = new System.Drawing.Size(75, 23);
+            this.btnHienthi.TabIndex = 4;
+            this.btnHienthi.Text = "Hiển Thị";
+            this.btnHienthi.UseVisualStyleBackColor = true;
+            this.btnHienthi.Click += new System.EventHandler(this.btnHienthi_Click_1);
+            // 
+            // cboKhoa
+            // 
+            this.cboKhoa.FormattingEnabled = true;
+            this.cboKhoa.Location = new System.Drawing.Point(63, 12);
+            this.cboKhoa.Name = "cboKhoa";
+            this.cboKhoa.Size = new System.Drawing.Size(170, 21);
+            this.cboKhoa.TabIndex = 5;
+            this.cboKhoa.SelectedIndexChanged += new System.EventHandler(this.cboKhoa_SelectedIndexChanged);
+            // 
+            // cboLop
+            // 
+            this.cboLop.FormattingEnabled = true;
+            this.cboLop.Location = new System.Drawing.Point(239, 11);
+            this.cboLop.Name = "cboLop";
+            this.cboLop.Size = new System.Drawing.Size(172, 21);
+            this.cboLop.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 430);
+            this.Controls.Add(this.cboLop);
+            this.Controls.Add(this.cboKhoa);
+            this.Controls.Add(this.btnHienthi);
+            this.Controls.Add(this.rdoSV);
+            this.Controls.Add(this.rdoLop);
+            this.Controls.Add(this.rdoKhoa);
+            this.Controls.Add(this.dgvKhoa);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "K48";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKhoa;
+        private System.Windows.Forms.RadioButton rdoKhoa;
+        private System.Windows.Forms.RadioButton rdoLop;
+        private System.Windows.Forms.RadioButton rdoSV;
+        private System.Windows.Forms.Button btnHienthi;
+        private System.Windows.Forms.ComboBox cboKhoa;
+        private System.Windows.Forms.ComboBox cboLop;
+
+    }
+}
+
---
+++ K48/K48/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace K48
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn;
+        SqlDataAdapter da;
+        string conmstring = @"Data Source=NHIHO-PC;Initial Catalog=K48;Integrated Security=True";
+        string SQL;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdoKhoa.Checked = true;
+            LayDLKhoa();
+
+        }
+
+        private void LayDL()
+        {
+            cn = new SqlConnection(conmstring);
+            da = new SqlDataAdapter(SQL, cn);
+            ds = new DataSet("Khoa");
+            da.Fill(ds, "Khoa");
+            dgvKhoa.DataSource = ds;
+            dgvKhoa.DataMember = "Khoa";
+            cn.Close();
+            cn.Dispose();
+        }
+
+        private void btnHienthi_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void LayDLKhoa()
+        {
+            cn = new SqlConnection(conmstring);
+            SQL = "SELECT * FROM Khoa";
+            da = new SqlDataAdapter(SQL, cn); //lấy dữ liệu về
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cboKhoa.DataSource = dt;//gán dữ liệu nguồn
+            cboKhoa.ValueMember = "MaKhoa";//dùng để lọc dữ liệu
+            cboKhoa.DisplayMember = "TenKhoa";//gán trường hiển thị trên combobox
+            cn.Close();
+            cn.Dispose();
+        }
+
+        private void LayDLLop()
+        {
+            cn = new SqlConnection(conmstring);
+            SQL = "SELECT * FROM Lop WHERE MaKhoa = N'" + cboKhoa.SelectedValue.ToString() + "'";//dữ liệu           
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(SQL, cn);
+            da.Fill(dt);
+            cboLop.DataSource = dt;
+            cboLop.ValueMember = "MaLop";
+            cboLop.DisplayMember = "TenLop";
+            cn.Close();
+            cn.Dispose();
+        }
+
+        private void cboKhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LayDLLop();
+        }
+
+        private void btnHienthi_Click_1(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(conmstring);
+            SQL = "SELECT * FROM SV WHERE MaLop = N'" + cboLop.SelectedValue.ToString() + "'";//dữ liệu
+            da = new SqlDataAdapter(SQL, cn);//cái lấy về
+            ds = new DataSet("Khoa");
+            da.Fill(ds, "Khoa");
+            dgvKhoa.DataSource = ds;
+            dgvKhoa.DataMember = "Khoa";
+            cn.Close();
+            cn.Dispose();
+        }
+
+        
+
+        
+
+
+    }
+        
+}
